2856a67199c2be386d3ff54b3ebca1991e3c800e
Aleksandar Ristevski <nice@ristevski.me>
2018-12-04 22:15:12 +0000

    Making Cookies Work

--- cookie-provider/app.js
+++ cookie-provider/app.js
@@ -1,37 +1,51 @@
 var express = require("express");
 var cookieParser = require("cookie-parser");
+var cors = require('cors');
 
 var port = 8888;
 
 var app = express();
+
+function getPayload() {
+    var randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2, randomNumber.length);
+    var payload = `|${randomNumber} # |This is some random payload # |111`;
+    return payload;
+}
+
+const corsOptions = {
+    exposedHeaders: 'X-My-Custom-Header, X-Another-Custom-Header, Fake-Cookie',
+    credentials: true,
+    origin: true,
+};
+
 app.use(cookieParser());
 app.use((req, res, next) => {
     var cookie = req.cookies.cookieName;
-    if (cookie === undefined)
-    {
-      var randomNumber=Math.random().toString();
-      randomNumber=randomNumber.substring(2, randomNumber.length);
-      var payload = `${randomNumber} # This is some random payload # 111`;
-      res.cookie('HungryCookie', payload, { maxAge: 900000, httpOnly: true });
-      console.log('cookie created');
-    } 
-    else
-    {
-      console.log('cookie exists', cookie);
-    } 
+    if (cookie === undefined) {
+        res.cookie('HungryCookie', getPayload(), { maxAge: 900000, httpOnly: true });
+        console.log('cookie created');
+    }
+    else {
+        console.log('cookie exists', cookie);
+    }
     next();
 });
+app.use(cors(corsOptions));
 
 app.get("/test", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+    res.set('X-My-Custom-Header', 'Baba Vanga');
+    res.set('Fake-Cookie', getPayload());
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
     next();
 });
 
 app.get("/gimmecookie", (req, res, next) => {
+    res.header
     res.json(["Test"]);
     next();
 });
 
 app.listen(port, () => {
- console.log("Server running on port 3000");
+    console.log(`Server running on port ${port}`);
 });
--- mickey/src/App.js
+++ mickey/src/App.js
@@ -1,25 +1,52 @@
+/* global document */
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
+const config = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Expose-Headers': 'Access-Token, Uid',
+  }
+};
+
 class App extends Component {
+  state = {
+    list: []
+  }
+
+  setCookie = (name, value, daysToExpiry) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (daysToExpiry * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value};${expires};path=/`;
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8888/test`)
+      .then(res => {
+        let fakecookie = res.headers['fake-cookie'];
+        console.log(fakecookie);
+        this.setCookie('MyMikeyCookie', fakecookie, 1);
+        const list = res.data;
+        this.setState({ list });
+      })
+      .catch((res) => {
+        console.log('Error', res);
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Some fancy code</h1>
+        <ul>
+          {this.state.list.map((item, i) => <li key={i}>{item}</li>)}
+        </ul>
       </div>
     );
   }
